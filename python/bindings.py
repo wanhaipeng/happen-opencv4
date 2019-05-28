ad12a1379a6dedf1b4ebf142d38b938e5d50b3b3
wanhaipeng <529057232@qq.com>
2019-05-28 16:05:36 +0800

    add pybind11 module && sample

---
+++ python/bindings.py
@@ -0,0 +1,14 @@
+import sys, os
+sys.path.insert(0, '../build/lib')
+import pybindings
+# Use the add function
+print("1 + 2 = {}".format(pybindings.add(1, 2)))
+
+# Use the adder class
+adder = pybindings.Adder(3)
+print("3 + 2 = {}".format(adder.add(2)))
+adder.addition = 4
+print("4 + 3 = {}".format(adder.add(2)))
+
+# Use the join function that uses the stl
+print("join(['a', 'b', 'c']) = {}".format(pybindings.join(['a', 'b', 'c'])))
